84e0641a5d3756feceae9ad74fd2446d10405d0f
Muhammad-Talha7 <naeemmuhammadtalha7@gmail.com>
2025-06-16 17:24:32 +0500

    Image slider with use state

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,35 @@ import { useState } from 'react'
 
 function App() {
 
-    const [Var,setVar] = useState(0)
-  
+  const [Count,setCount]= useState(0);
+   const images = [
+  {
+    src: "https://plus.unsplash.com/premium_photo-1664303847960-586318f59035?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+]
+const decrement = ()=>{
+  setCount((count)=> (count +1 )%images.length)
+}
   return (
  <>
- <h1>{Var}</h1>
- <button onClick={()=>{setVar(Var+1)}}>Add into var </button>
+
     <Router>
          <h1>
          <Navbar/>
+         <button onClick={decrement}>Level</button>
+         <img src={images[Count].src} alt="Image not shown due to some reason" />
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>}/>
